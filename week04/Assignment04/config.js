ab295617c4c042aa0ce170a727f00d1d5b6b1c83
Kienn Thiett <smoothkt4951@gmail.com>
2021-08-07 15:03:17 +0700

    clientID and App secrect for Facebook OAuth

--- week04/Assignment04/config.js
+++ week04/Assignment04/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongourl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientID: '977497216057981',
-        clientSecret: 'ed33c4e0ed916449a24e43a594b15962'
+        clientID: '1424065794637404',
+        clientSecret: '5d767004637acf7ac4240ae29cf0a3ee'
     }
 }
